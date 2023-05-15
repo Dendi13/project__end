7b2fe746692f0f071a6badd3bed23355a0aadd10
Dendi13 <98680907+Dendi13@users.noreply.github.com>
2023-05-15 14:52:24 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
 let myBtn = document.querySelector('#myBtn');
 let input__list = document.querySelector('#input__list');
-let input__price = document.querySelector('#input__price')
+let input__price = document.querySelector('#input__price');
+let select__category = document.querySelector('#select__category');
+let list = document.querySelector('#list');
 
 myBtn.addEventListener('click', (evt) => {
     evt.preventDefault();
     let name = input__list.value;
-    let prise = input__price.value
-    console.log(name, prise)
+    let prise = input__price.value;
+    let category = select__category.value;
+    console.log(name, prise, category);
+
+    let newLi = document.createElement('li');
+   
+    list.append(newLi);
+    newLi.className = 'newLi'
+
 })
