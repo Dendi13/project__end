eaa77842ab86eb9d2361729ad9f36ac861aa6fd9
Dendi13 <98680907+Dendi13@users.noreply.github.com>
2023-05-15 18:02:16 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -2,18 +2,22 @@ let myBtn = document.querySelector('#myBtn');
 let input__list = document.querySelector('#input__list');
 let input__price = document.querySelector('#input__price');
 let select__category = document.querySelector('#select__category');
-let list = document.querySelector('#list');
+let table = document.querySelector('#table')
 
 myBtn.addEventListener('click', (evt) => {
     evt.preventDefault();
     let name = input__list.value;
     let prise = input__price.value;
     let category = select__category.value;
-    console.log(name, prise, category);
 
-    let newLi = document.createElement('li');
-   
-    list.append(newLi);
-    newLi.className = 'newLi'
+    let newTr = document.createElement('tr');
+    table.append(newTr);
+    newTr.classList.add('newTr');
+
+    let newTd = document.createElement('td');
+    newTr.append(newTd);
+    newTd.classList.add('newTd');
+
+    newTr.textContent = `${name} ${price}`
 
 })
